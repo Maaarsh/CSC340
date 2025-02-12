3bfb61ffe421a6ed71f77ddfcebe10fcb85d876c
Maaarsh <marshallstoro@gmail.com>
2025-02-12 13:30:32 -0800

    runs but only with commands in termianak
    run this command first:
    g++ -o milestone1 milestone1.cpp doubly_linked_list.cpp dll_node.cpp generate_output.cpp
    
    then this command:
    ./milestone1

--- Milestone1/doubly_linked_list.cpp
+++ Milestone1/doubly_linked_list.cpp
@@ -15,255 +15,180 @@ Created with the help of the following resources:
 DoublyLinkedList::DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
 // Destructor: Clears the list
-DoublyLinkedList::~DoublyLinkedList()
-{
-  clear();
+DoublyLinkedList::~DoublyLinkedList() {
+    clear();
 }
 
-// replace Node with DllNode
-
-bool DoublyLinkedList::isEmpty() // needs to empty in ()
-{
-  if (head == nullptr)
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+// Method to check if the list is empty
+bool DoublyLinkedList::isEmpty() {
+    return head == nullptr;
 }
 
-void DoublyLinkedList::insertAtHead(int key) // int key parameter only
-{
-  DllNode *newDllNode = new DllNode(key); // Create a new node with the given value
+// Method to insert a new node at the head of the list
+void DoublyLinkedList::insertAtHead(int key) {
+    DllNode* newDllNode = new DllNode(key); // Create a new node with the given value
 
-  if (isEmpty()) // If the list is empty then the new node is the head
-  {
-    head = newDllNode;
-    return;
-  }
-  else // Not needed but I like it for readability
-  {
-    // Inserting at head so we put the new node at the front of the list so...
-    newDllNode->next = head; // point the new node to the current head (Soon to be old head/second node)...
-    head->prev = newDllNode; // then point the current head (Soon to be old head/second node) back to the new node because doubly linked list goes both ways...
-    head = newDllNode;       // then set the new node as the head.
-    return;
-  }
+    if (isEmpty()) { // If the list is empty then the new node is the head
+        head = tail = newDllNode;
+    } else {
+        newDllNode->next = head; // Point the new node to the current head
+        head->prev = newDllNode; // Point the current head back to the new node
+        head = newDllNode;       // Set the new node as the head
+    }
 }
 
-void DoublyLinkedList::insertAtTail(int key)
-{
-  DllNode *newDllNode = new DllNode(key); // Create a new node with the given value.
+// Method to insert a new node at the tail of the list
+void DoublyLinkedList::insertAtTail(int key) {
+    DllNode* newDllNode = new DllNode(key); // Create a new node with the given value
 
-  if (isEmpty())
-  {
-    head = newDllNode;
-    return;
-  }
-  else
-  {
-    // Find the last node in the list
-    DllNode *temp = head;
-    while (temp->next != nullptr)
-    {
-      temp = temp->next;
+    if (isEmpty()) {
+        head = tail = newDllNode;
+    } else {
+        tail->next = newDllNode; // Point the current tail to the new node
+        newDllNode->prev = tail; // Point the new node back to the current tail
+        tail = newDllNode;       // Set the new node as the tail
+    }
+}
+
+// Method to remove the header node (first node)
+void DoublyLinkedList::removeHeaderNode() {
+    if (isEmpty()) {
+        std::cout << "List is empty" << '\n';
+        return;
+    } else {
+        DllNode* temp = head; // Create a temporary node to store the head
+        head = head->next;    // Set the head to the next node
+        if (head != nullptr) {
+            head->prev = nullptr; // Set previous node of the new head to null
+        } else {
+            tail = nullptr; // If the list is now empty, set tail to nullptr
+        }
+        delete temp;
+    }
+}
+
+// Method to remove the tail node (last node)
+void DoublyLinkedList::removeTailNode() {
+    if (isEmpty()) {
+        std::cout << "List is empty" << '\n';
+        return;
+    } else if (head->next == nullptr) { // If the list has only one node
+        delete head;
+        head = tail = nullptr;
+    } else {
+        DllNode* temp = tail; // Create a temporary node to store the tail
+        tail = tail->prev;    // Set the tail to the previous node
+        tail->next = nullptr; // Set next node of the new tail to null
+        delete temp;
+    }
+}
+
+// Method to remove a node with a specific value from the list
+void DoublyLinkedList::remove(int key) {
+    if (isEmpty()) {
+        std::cout << "List is empty" << '\n';
+        return;
+    } else {
+        DllNode* temp = head;
+        while (temp != nullptr && temp->key != key) { // Find the node with the value or the end of the list
+            temp = temp->next;
+        }
+
+        if (temp == nullptr) {
+            std::cout << "Value not found" << '\n';
+            return;
+        } else if (temp == head) {
+            removeHeaderNode();
+        } else if (temp == tail) {
+            removeTailNode();
+        } else {
+            temp->prev->next = temp->next; // Point the node before temp to the node after temp
+            temp->next->prev = temp->prev; // Point the node after temp to the node before temp
+            delete temp;
+        }
+    }
+}
+
+// Method to move a specific node to the head of the list
+void DoublyLinkedList::moveNodeToHead(int key) {
+    if (head == nullptr || key < 1) {
+        std::cout << "Invalid input or list is empty" << '\n';
+        return;
     }
 
-    // Insert the new node at the end of the list
-    // temp->next = newNode; // Point the last node to the new node. DONT NEED THIS NEW NODE
-    newDllNode->prev = temp; // Point the new node back to the last node
-    return;
-  }
+    DllNode* current = head;
+    DllNode* prevKth = nullptr;
+
+    while (current != nullptr && current->key != key) {
+        prevKth = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        std::cout << "Node not found" << '\n';
+        return;
+    }
+
+    if (current == head) {
+        return; // Node is already at the head
+    }
+
+    if (prevKth != nullptr) {
+        prevKth->next = current->next; // Point the node before the kth node to the node after the kth node
+    }
+    if (current->next != nullptr) {
+        current->next->prev = prevKth; // Point the node after the kth node to the node before the kth node
+    }
+
+    current->next = head;    // Make the current node point to the head
+    head->prev = current;    // Make the head point to the current node
+    current->prev = nullptr; // Make the current node point to null
+    head = current;          // Make the current node the head
 }
 
-void DoublyLinkedList::removeHeaderNode()
-{
-  if (isEmpty())
-  {
-    std::cout << "List is empty" << '\n';
-    return; // If the list is empty then there is nothing to remove
-  }
-  else
-  {
-    DllNode *temp = head; // Create a temporary node to store the head
-    head = head->next;    // Set the head to the next node
-    head->prev = nullptr; // Set previous node of the new head to null
-    return;
-  }
+// Method to move a specific node to the tail of the list
+void DoublyLinkedList::moveNodeToTail(int key) {
+    if (head == nullptr || key < 1) {
+        std::cout << "Invalid input or list is empty" << '\n';
+        return;
+    }
+
+    DllNode* current = head;
+    DllNode* prevKth = nullptr;
+
+    while (current != nullptr && current->key != key) {
+        prevKth = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        std::cout << "Node not found" << '\n';
+        return;
+    }
+
+    if (current == tail) {
+        return; // Node is already at the tail
+    }
+
+    if (prevKth != nullptr) {
+        prevKth->next = current->next; // Point the node before the kth node to the node after the kth node
+    }
+    if (current->next != nullptr) {
+        current->next->prev = prevKth; // Point the node after the kth node to the node before the kth node
+    }
+
+    current->next = nullptr;
+    current->prev = tail;
+    tail->next = current;
+    tail = current;
 }
 
-void DoublyLinkedList::removeTailNode()
-{
-  DllNode *temp = head;
-  // If the list is empty then there is nothing to remove
-  if (isEmpty())
-  {
-    std::cout << "List is empty" << '\n';
-    return;
-  }
-  // If the list has only one node then delete the node and set the head to nullptr
-  else if (head->next == nullptr)
-  {
-    DllNode *temp = head;
-    head = nullptr;
-    delete head;
-    return;
-  }
-  else
-  {
-    // Find the second to last node in the list
-    while (temp->next != nullptr)
-    {
-      temp = temp->next;
+// Method to clear the list by deleting all nodes
+void DoublyLinkedList::clear() {
+    while (head != nullptr) {
+        DllNode* temp = head;
+        head = head->next;
+        delete temp;
     }
-    temp->prev->next = nullptr; // Point the second to last node to null
-    int key = temp->key;
-    delete temp;
-    return;
-  }
-}
-
-void DoublyLinkedList::remove(int key) // remove a node with a specific value
-{
-  DllNode *temp = head;
-
-  // If the list is empty then there is nothing to remove
-  if (isEmpty())
-  {
-    std::cout << "List is empty" << '\n';
-    return;
-  }
-  else
-  {
-    // If the value is in the head node then remove the head node
-    if (head->key == key)
-    {
-      removeHeaderNode();
-      return;
-    }
-
-    while (temp != nullptr && temp->key != key) // Find the node with the value or the end of the list
-    {
-      temp = temp->next;
-    }
-
-    // If the value is not found then print a message
-    if (temp == nullptr)
-    {
-      std::cout << "Value not found" << '\n';
-      return;
-    }
-    // If the value is in the last node then remove the last node
-    else if (temp->next == nullptr)
-    {
-      removeTailNode();
-      return;
-    }
-    // If the value is somewhere in the middle of the list then remove the node
-    else
-    {
-      // Node *temp2 = temp;
-      temp->prev->next = temp->next; // Point the node before temp to the node after temp
-      temp->next->prev = temp->prev; // Point the node after temp to the node before temp
-      delete temp;
-      return;
-    }
-  }
-}
-
-void DoublyLinkedList::moveNodeToHead(int key)
-{
-  // If the list is empty then there is nothing to move or if k is less than 1 it is an invalid input or if k is 1 then the node is already at the head
-  if (head == nullptr || key < 1 || key == 1)
-  {
-    std::cout << "Invalid input or list is empty" << '\n';
-    return;
-  }
-
-  DllNode *current = head;
-  DllNode *prevKth = nullptr;
-
-  for (int i = 1; current != nullptr && i < key; i++) // for loop until current is not null and i is greater than k to find the kth node
-  {
-    prevKth = current;
-    current = current->next;
-  }
-
-  if (current == nullptr) // If the kth node is not found then return
-  {
-    std::cout << "Node not found" << '\n';
-    return;
-  }
-  if (prevKth != nullptr) // If the kth node is not the head node then...
-  {
-    prevKth->next = current->next; // ...point the node before the kth node to the node after the kth node
-  }
-  if (current->next != nullptr) // If the kth node is not the last node then...
-  {
-    current->next->prev = prevKth; // ...point the node after the kth node to the node before the kth node
-  }
-
-  current->next = head;    // Make the current node point to the head
-  head->prev = current;    // Make the head point to the current node
-  current->prev = nullptr; // Make the current node point to null
-  head = current;          // Make the current node the head
-  return;
-}
-
-void DoublyLinkedList::moveNodeToTail(int key)
-{
-  // If the list is empty then there is nothing to move or if k is less than 1 it is an invalid input
-  if (head == nullptr || key < 1)
-  {
-    std::cout << "Invalid input or list is empty" << '\n';
-    return;
-  }
-
-  DllNode *current = head;
-  DllNode *prevKth = nullptr;
-
-  for (int i = 1; current != nullptr && i < key; i++) // for loop until current is not null and i is greater than k to find the kth node
-  {
-    prevKth = current;
-    current = current->next;
-  }
-
-  if (current == nullptr) // If the kth node is not found then return
-  {
-    std::cout << "Node not found" << '\n';
-    return;
-  }
-  if (prevKth != nullptr) // If the kth node is not the head node then...
-  {
-    prevKth->next = current->next; // ...point the node before the kth node to the node after the kth node
-  }
-  if (current->next != nullptr) // If the kth node is not the last node then...
-  {
-    current->next->prev = prevKth; // ...point the node after the kth node to the node before the kth node
-  }
-
-  insertAtTail(current->key); // Insert the kth node at the tail
-  return;
-}
-
-void DoublyLinkedList::clear()
-{
-  // If the list is empty then there is nothing to clear
-  if (isEmpty())
-  {
-    std::cout << "List is empty" << '\n';
-    return;
-  }
-  else
-  {
-    // Loop through the list and delete each node
-    while (head != nullptr)
-    {
-      DllNode *temp = head;
-      head = head->next;
-      delete temp;
-    }
-  }
+    tail = nullptr;
 }
--- Milestone1/generate_output.cpp
+++ Milestone1/generate_output.cpp
@@ -8,141 +8,42 @@ Created with the help of the following resources:
 */
 
 #include <iostream>
-// #include "doubly_linked_list.cpp"
 #include "doubly_linked_list.h"
 #include "generate_output.h"
 #include "dll_node.h"
 
-void printList(DoublyLinkedList FIFOList)
+void printList(DoublyLinkedList &FIFOList)
 {
-  DllNode *temp = FIFOList.head;
-  std::cout << "Forward List:" << '\n';
-  while (temp != nullptr)
-  {
-    std::cout << temp->key << " ";
-    temp = temp->next;
-  }
-  std::cout << '\n';
+    DllNode *temp = FIFOList.head;
+    std::ofstream &outFile = getOutFile(); // Get the reference to the output file stream
+    // std::cout << "Forward List:" << '\n';
+    // outFile << "Forward List:" << '\n';
+    while (temp != nullptr)
+    {
+        std::cout << temp->key << " ";
+        outFile << temp->key << " "; // Write to output file
+        temp = temp->next;
+    }
+    std::cout << '\n';
+    outFile << '\n'; // Write a newline to the file
 }
-// {
-//   Node *temp = head;
-//   std::cout << "Forward List:" << '\n';
-//   while (temp != nullptr)
-//   {
-//     std::cout << temp->val << " ";
-//     temp = temp->next;
-//   }
-//   std::cout << '\n';
-// }
 
-void reversePrintList(DoublyLinkedList FIFOList)
+void reversePrintList(DoublyLinkedList &FIFOList)
 {
-  DllNode *temp = FIFOList.head;
-  std::cout << "Reverse List:" << '\n';
-  while (temp->next != nullptr)
-  {
-    temp = temp->next;
-  }
-  while (temp != nullptr)
-  {
-    std::cout << temp->key << " ";
-    temp = temp->prev;
-  }
-  std::cout << '\n';
+    DllNode *temp = FIFOList.head;
+    std::ofstream &outFile = getOutFile(); // Get the reference to the output file stream
+    // std::cout << "Reverse List:" << '\n';
+    // outFile << "Reverse List:" << '\n';
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    while (temp != nullptr)
+    {
+        std::cout << temp->key << " ";
+        outFile << temp->key << " "; // Write to output file
+        temp = temp->prev;
+    }
+    std::cout << '\n';
+    outFile << '\n'; // Write a newline to the file
 }
-// {
-//   Node *temp = head;
-//   std::cout << "Reverse List:" << '\n';
-//   while (temp->next != nullptr)
-//   {
-//     temp = temp->next;
-//   }
-//   while (temp != nullptr)
-//   {
-//     std::cout << temp->val << " ";
-//     temp = temp->prev;
-//   }
-//   std::cout << '\n';
-// }
-
-// int main()
-// {
-//   Node *head = nullptr;
-
-//   // Check all functions
-
-//   std::cout << "isEmpty: ";
-//   if (isEmpty(head))
-//   {
-//     std::cout << "True" << '\n';
-//   }
-//   else
-//   {
-//     std::cout << "False" << '\n';
-//   }
-
-//   std::cout << "Inserted at head: " << insertAtHead(head, 50) << '\n';
-//   std::cout << "Inserted at head: " << insertAtHead(head, 40) << '\n';
-//   std::cout << "Inserted at head: " << insertAtHead(head, 30) << '\n';
-
-//   std::cout << "Inserted at tail: " << insertAtTail(head, 60) << '\n';
-//   std::cout << "Inserted at tail: " << insertAtTail(head, 70) << '\n';
-//   std::cout << "Inserted at tail: " << insertAtTail(head, 80) << '\n';
-//   std::cout << "Inserted at tail: " << insertAtTail(head, 90) << '\n';
-
-//   std::cout << "Inserted at head: " << insertAtHead(head, 20) << '\n';
-//   std::cout << "Inserted at head: " << insertAtHead(head, 10) << '\n';
-
-//   std::cout << "Inserted at tail: " << insertAtTail(head, 100) << '\n';
-
-//   std::cout << "isEmpty: ";
-//   if (isEmpty(head))
-//   {
-//     std::cout << "True" << '\n';
-//   }
-//   else
-//   {
-//     std::cout << "False" << '\n';
-//   }
-
-//   printListForward(head);
-
-//   reversePrintList(head);
-
-//   std::cout << "Removed header node: " << removeHeaderNode(head) << '\n';
-
-//   std::cout << "Removed tail node: " << removeTailNode(head) << '\n';
-
-//   std::cout << "Removed node with value 50: " << remove(head, 50) << '\n';
-
-//   printListForward(head);
-
-//   reversePrintList(head);
-
-//   std::cout << "Moved 3rd node to the head: " << moveNodeToHead(head, 3) << '\n';
-
-//   printListForward(head);
-
-//   reversePrintList(head);
-
-//   std::cout << "Moved 3rd node to the tail: " << moveNodeToTail(head, 3) << '\n';
-
-//   printListForward(head);
-
-//   reversePrintList(head);
-
-//   clear(head);
-//   std::cout << "Cleared list" << '\n';
-
-//   std::cout << "isEmpty: ";
-//   if (isEmpty(head))
-//   {
-//     std::cout << "True" << '\n';
-//   }
-//   else
-//   {
-//     std::cout << "False" << '\n';
-//   }
-
-//   return 0;
-// }
